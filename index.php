d3c4572477dd8fd4f5837936031bc86c781cda38
GokuFase3 <105016625+GokuFase3@users.noreply.github.com>
2023-02-11 21:24:15 -0600

    V1.0 PHP como mandar alertas en la pagina
    V1.0 PHP como mandar alertas en la pagina

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Evento</title>
+</head>
+<body>
+    <script>
+         function alerta(){
+             alert("Holaaaaaaaa");
+         }
+    </script>
+    <fieldset>
+    	<legend>Evento en un click para mandar correos</legend>
+    	<input type="button" onclick="alerta()" id="boton" value="aletrta">
+
+    </fieldset>
+
+</body>
+
+</html>
+
 <?php
-
-$ID = $_GET['id'];
-#$DATE = "2022-11-16 17:05:13";
-
-echo "El UID registrado es: " .$ID;
-#echo "<br>La fecha es:" .$DATE;
-
-$usuario = "root";
-$contrasena = "";
-$servidor = "localhost";
-$basededatos = "aulas";
-
-$conexion = mysqli_connect($servidor, $usuario, "") or die ("No se ha podido conectar al servidor de la Base de datos");
-
-$db = mysqli_select_db($conexion, $basededatos) or die ("No se ha podido seleccionar la Base de datos");
-
-#$fecha = time();
-#".$ID."
-$consulta = "INSERT INTO sala1 (id) VALUES ('$ID')";
-
-$resultado = mysqli_query($conexion, $consulta) or die ("No se pudo subir los datos");
 
 ?>
