b8a04e4229cece7b5001246bf4699a4f4ad88cc4
GokuFase3 <105016625+GokuFase3@users.noreply.github.com>
2023-02-11 21:26:24 -0600

    V2.0 PHP registrar usuarios desde servidor
    V2.0 PHP registrar usuarios desde servidor

--- pedir.php
+++ pedir.php
@@ -2,26 +2,23 @@
 <html>
 <head>
    <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="estiloR.css">
    <title>RegistroUID</title>
 </head>
 <body>
+<nav id="Nav">
    <H1>BIENVENIDO!</H1>
-   <H3>Ingrese su datos por favor</H3>
+</nav>
    <form action="subir.php" methot="get">
+         <h1 id="Navh1"> Ingrese su datos por favor </h1>
        	Nombre: <input type="text" name="name">
-   	   
-         <br><br><br>
+         <br><br>
          Contacto: <input type="text" name="inf_cont">
-
-         <H3>Ingrese el UID de la tarjeta que le proporcionaron</H3>
-
+         <h1 id="Navh1">Ingrese el UID de la tarjeta que le proporcionaron</h1>
          UID: <input type="text" name="tarjeta">
          
          <br><br><br>
          <input type="submit" value="Enviar">
-
    </form>
 </body>
 </html>
-
-
--- subir.php
+++ subir.php
@@ -1,17 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="estiloR.css">
+   <title>RegistroUID</title>
+</head>
+<body>
+<h1 id="Navh1"> </h1>
 <?php
 
 if ($_GET){
 
-$UID = $_GET['tarjeta'];
-$user = $_GET['name'];
-$mensaje = $_GET['inf_cont'];
-
-        echo "Su nombre es: " .$_GET["name"];
-
-        echo "<br><br>Su contacto es: " .$_GET["inf_cont"];
-
-        echo "<br><br>El UID de su tarjeta es: " .$_GET["tarjeta"];
-   }else{
+      $UID = $_GET['tarjeta'];
+      $user = $_GET['name'];
+      $form_contacto = $_GET['inf_cont'];
+}else{
         echo "Sin datos";
    }
 
@@ -24,7 +27,24 @@ $conexion = mysqli_connect($servidor, $usuario, "") or die ("No se ha podido con
 
 $db = mysqli_select_db($conexion, $basededatos) or die ("No se ha podido seleccionar la Base de datos");
 
-$consulta = "INSERT INTO info (id, nombre, contacto) VALUES ('$UID','$user','$mensaje')";
-$resultado = mysqli_query($conexion, $consulta) or die ("No se subir los datos");
+$consulta = "SELECT EXISTS (SELECT id FROM info WHERE id = '$UID');";
 
+$resultado = mysqli_query($conexion, $consulta);
+$row = mysqli_fetch_row($resultado);
+if($row[0] == FALSE){
+     printf("\r");
+      echo "<div align=\"center\"><H1>Datos subidos con exito</H1></div>";
+      echo "<br><br><div align=\"center\">Su nombre es: $user </div>";
+      echo "<br><br><div align=\"center\">Su contacto es: $form_contacto </div>";
+      echo "<br><br><div align=\"center\">El UID de su tarjeta es: $UID </div> ";
+
+      $subir = "INSERT INTO info (id, nombre, contacto) VALUES ('$UID','$user','$form_contacto')";
+      $new_R = mysqli_query($conexion, $subir);
+}else{
+      echo "<div align=\"center\"><H1>Error fatal!!!</H1></div> ";
+      echo "<div align=\"center\"><br><br>No se han podido subir los datos</div> ";
+      echo "<div align=\"center\"><br><br>Favor de ponerse en contacto con el administrador</div> ";
+}
 ?>
+</body>
+</html>
